f6f0baa3247cfee155623b2109c044bbf17207a5
Simone Fagini <127838478+simonefagini@users.noreply.github.com>
2024-12-05 17:17:04 +0100

    Update template_cmd.py
    Added description and general error handling.

--- commands/template_cmd.py
+++ commands/template_cmd.py
@@ -1,10 +1,23 @@
-import rhinoscriptsyntax as rs
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
-__commandname__ = "template"
+"""
+template_cmd.py
+Description...
+Written by simone fagini as part of Fluo-for-Rhino (https://github.com/simonefagini/Fluo-for-Rhino/)
+date in place, GPL3.0
+"""
+
+import rhinoscriptsyntax as rs
+#import library
+
+__commandname__ = "Template"
+
 
 def template():
       # Main code of command
-      print("TEST")
+      print(f"Running {__commandname__}...")
+      
       return
 
 # RunCommand is the called when the user enters the command name in Rhino.
@@ -20,6 +33,12 @@ def RunCommand( is_interactive ):
   #   1 == cancel
   # If this function does not return a value, success is assumed
   return 0
-
+    
 if __name__ == "__main__":
-    template()
+     
+    try:
+        template()
+    except ValueError as e:
+        print e
+    except Exception:
+        print("Something went wrong...")
