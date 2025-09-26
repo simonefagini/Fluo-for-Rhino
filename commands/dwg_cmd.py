bd98985a1c6d2246e229e431e0e120b96f1bbea8
Simone Fagini <127838478+simonefagini@users.noreply.github.com>
2025-09-26 09:30:31 +0200

    Update dwg_cmd.py
    _-ExportAll is not supported anymore in Rhino 8.. so worked around with different command which reshuffled the all command workflow.

--- commands/dwg_cmd.py
+++ commands/dwg_cmd.py
@@ -15,6 +15,7 @@ def toDWG():
     SAVE_SMALL = "No"
     GEOMETRY_ONLY = "Yes"
     SAVE_TEXTURES = "No"
+    SAVE_NOTES = "No"
     SAVE_PLUGIN_DATA = "No"
     
     doc_path = rs.DocumentPath()
@@ -26,10 +27,10 @@ def toDWG():
     
     base_name = doc_name[:-4]
     dwg_file = chr(34) + doc_path + base_name + '.dwg' + chr(34)
-    print("Exporting to: " + dwg_file)
+    print("Exporting to DWG 2007 Solids...")
     
-    settings = "_Version=" + DWG_VERSION + " _SaveSmall=" + SAVE_SMALL + " _GeometryOnly=" + GEOMETRY_ONLY + " _SaveTextures=" + SAVE_TEXTURES + " _SavePlugInData=" + SAVE_PLUGIN_DATA
-    command = "_NoEcho _-ExportAll " + dwg_file + " " + settings + " _Enter"
+    settings = " _Version=" + DWG_VERSION + " _SaveSmall=" + SAVE_SMALL + " _GeometryOnly=" + GEOMETRY_ONLY + " _SaveTextures=" + SAVE_TEXTURES + " _SaveNotes=" + SAVE_NOTES + " _SavePlugInData=" + SAVE_PLUGIN_DATA + " "
+    command = "_SelNone _-ExportWithOrigin _SelAll _Enter _Enter " + settings + dwg_file + " _Enter _SelNone"
     
     rs.Command(command)
 
