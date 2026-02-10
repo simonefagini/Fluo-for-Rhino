ee5eca317d7513fee02533115e7949621f498f13
Simone Fagini <127838478+simonefagini@users.noreply.github.com>
2026-02-10 09:59:12 +0100

    Add files via upload

---
+++ scripts/blocksRenamer.py
@@ -0,0 +1,21 @@
+import rhinoscriptsyntax as rs
+
+def getPrefix():
+    prefix = rs.GetString("Insert Prefix")
+    
+    if prefix is None:
+       return
+    
+    return prefix
+
+def batchRenamer():
+    prefix = getPrefix()
+    oldBlockNames = rs.BlockNames(True)
+        
+    if oldBlockNames:
+        for name in oldBlockNames:
+            newBlockName= (prefix + name)
+            rs.RenameBlock (name,newBlockName)
+    return
+
+batchRenamer()
